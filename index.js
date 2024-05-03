d15610bf5fcfbfc1ea1b7c7950e96058c6c5be6d
Tebbo <leetebbs@ymail.com>
2024-05-03 14:06:39 +0100

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const data = {
+    "userAccount": "0x1ABc133C222a185fEde2664388F08ca12C208F76",
+    "powerGenerated": 1000,
+    "powerConsumed": 800,
+}
+
+
+app.get('/', (req, res) => {
+    res.send(data);
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+})
